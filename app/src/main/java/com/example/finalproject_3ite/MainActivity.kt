a7d5cc813135097a3c48ea55d41e272f1de7b138
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2023-11-21 10:50:26 +0800

    Added eclipse

--- app/src/main/java/com/example/finalproject_3ite/MainActivity.kt
+++ app/src/main/java/com/example/finalproject_3ite/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.finalproject_3ite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
+
     }
 }
